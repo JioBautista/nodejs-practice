cdff8de19874e2c5e00655c81a4ea982cb6cd3d3
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2023-12-21 19:21:28 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-const data = require('./userNames')
-const firstNames = data.users.map(item => item.firstName);
-const lastNames = data.users.map(item => item.lastName)
-const displayNames = require('./utils')
+const data = require("./userNames");
+const firstNames = data.users.map((item) => item.firstName);
+const lastNames = data.users.map((item) => item.lastName);
+const displayNames = require("./utils");
 
-displayNames(firstNames[0])
-displayNames(firstNames[1])
-displayNames(firstNames[2])
-displayNames(lastNames[0])
+displayNames(firstNames[0]);
+displayNames(firstNames[1]);
+displayNames(firstNames[2]);
+displayNames(lastNames[0]);
+
+const { readFile } = require("fs");
+
+readFile('./userNames.js','utf8', (err,data) => {
+    if(err) {
+        console.log(err);
+        return;
+    }
+    console.log(data);    
+})
