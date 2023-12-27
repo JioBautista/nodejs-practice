044059bbde0eb23268f1f7fbfd70349fcb916de4
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2023-12-26 19:27:48 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,17 @@
-const data = require("./userNames");
-const firstNames = data.users.map((item) => item.firstName);
-const lastNames = data.users.map((item) => item.lastName);
-const displayNames = require("./utils");
-
-displayNames(firstNames[0]);
-displayNames(firstNames[1]);
-displayNames(firstNames[2]);
-displayNames(lastNames[0]);
-
 const { readFile } = require("fs");
 
-readFile("./userNames.js", "utf8", (err, data) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log(data);
-});
+const getData = (path) => {
+  return new Promise((resolve, reject) => {
+    readFile(path, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
 
-const http = require("http");
-const server = http.createServer((req, res) => {
-    if(req.url === "/") {
-        res.end('<h1>Welcome to my page</>')
-    } if (req.url === "/about") {
-        res.end('<h1>My name is JIO!</>')
-    }
-});
-server.listen(5000);
-console.log("Start");
-
-setTimeout(() => {
-  console.log("5 seconds")  
-}, 10000);
-
-console.log("End")
+getData("./userNames.js")
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
