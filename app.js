55ce58794ea63bc5ec9cb15c64726b88e9aa0eb9
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-01-03 10:43:01 -0500

    cc

--- app.js
+++ app.js
@@ -1,22 +1,6 @@
-const { readFile, writeFile } = require("fs").promises;
-const EventEmitter = require('events')
+const express = require('express')
+const app = express()
 
-const customEmitter = new EventEmitter()
-
-const start = async () => {
-  try {
-    const getData = await readFile("./userNames.js", "utf8");
-    console.log(getData);
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-customEmitter.on('hasLoggedIn', (name,id) => {
-  console.log(`${name} with ID of ${id} has succesfully logged in.`)
+app.listen(5000,() => {
+  console.log('Server is listening on port 5000')
 })
-
-customEmitter.emit('hasLoggedIn','John',24)
-customEmitter.emit('hasLoggedIn','Andrew',14)
-
-start();
