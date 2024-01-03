89e3fc98abdb7ed8a8d72754f5511e57465e1995
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-01-03 11:05:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.listen(5000,() => {
-  console.log('Server is listening on port 5000')
-})
+
+app.get("/", (req, res) => {
+  res.status(200).send("Home Page");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>404 NOT FOUND <h1/>");
+});
+
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000");
+});
